02c97613a8ea62e4ec305dbe734447845a50387c
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 20:07:18 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -44,7 +44,7 @@ def play(board)
   while turns.length > 0
     spent_index = turn(board)
     puts "SPENT_INDEX", spent_index
-    
+    puts "turns.length: ",turns.length
     # remove the spent_index
     turns.delete(spent_index)
   end
